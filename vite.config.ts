b4aad2177b373e184c33cc90805cdd583245c5d6
chuck yin <yyin@me.com>
2025-08-02 17:55:36 -0700

    Configure for custom domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/robo-craft-future/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
